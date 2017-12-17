698f661faea77345aa6d8f571a414ce573ef8391
Guanzhou Zhao <benzhao2866@gmail.com>
2017-12-17 20:29:40 +1300

    server listen to 0.0.0.0

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 var app = require('./app')
 var port = process.env.PORT || 3000
 
-app.listen(port, process.env.DB_HOST, function () {
-  console.log(`listening on: ${port} HOST:NODE_ENV: ${process.env.DB_HOST}:${process.env.NODE_ENV}`)
+app.listen(port, process.env.HOST, function () {
+  console.log(`listening on: ${port} HOST:NODE_ENV: ${process.env.HOST}:${process.env.NODE_ENV}`)
 })
