ea3c15b723c9c68970512124b8c96bad05214f28
Guanzhou Zhao <benzhao2866@gmail.com>
2017-12-17 20:23:26 +1300

    add host specification when start server

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 var app = require('./app')
 var port = process.env.PORT || 3000
 
-app.listen(port, function () {
-  console.log(`listening on: ${port} NODE_ENV: ${process.env.NODE_ENV}`)
+app.listen(port, process.env.DB_HOST, function () {
+  console.log(`listening on: ${port} HOST:NODE_ENV: ${process.env.DB_HOST}:${process.env.NODE_ENV}`)
 })
