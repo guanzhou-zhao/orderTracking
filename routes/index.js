0b71b5b6d8d0370d0f681579f89eb972d08f7299
Guanzhou Zhao <benzhao2866@gmail.com>
2017-12-22 22:47:46 +1300

    move order relavant functions into separate router

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
 var log = require('debug')('routes:index:log')
 var _ = require('lodash')
 var User = require('../models/user')
-var Order = require('../models/order')
+
 var taskRouter = require('./task')
-var moment = require('moment');
+var orderRouter = require('./order')
 module.exports = router
 
 router.get('/login', function (req, res, next) {
@@ -27,32 +27,14 @@ router.get('/register', function (req, res) {
 })
 
 router.use(ensureLoggedIn())
+
 router.get('/', function (req, res) {
   res.redirect('/order')
 });
 router.use('/task', taskRouter)
+router.use('/order', orderRouter)
 
-router.get('/order', function (req, res) {
-  console.log('req.query: ', req.query)
-  var isAdmin = req.user.username == 'rosfiled'
-  var data = {
-    error: req.flash('error'),
-    isAdmin,
-    user: req.user,
-    isOrder: true
-  }
-  Order.getByUsername(req.user.username, isAdmin).then((orders) => {
-    var ordersDate = orders.map(order => {
-      return moment(order.created_at).format('YYYY')
-    })
-    console.log(`order dates: ${JSON.stringify(ordersDate)}`)
-    data.orders = orders.map(order => {
-      order.created_at = moment(order.created_at).format('YYYY-MM-D HH:mm:ss')
-      return order
-    });
-    res.render('order', data)
-  })
-});
+
 router.get('/user', function (req, res) {
   console.log('req.query: ', req.query)
   var isAdmin = req.user.username == 'rosfiled'
@@ -67,7 +49,7 @@ router.get('/user', function (req, res) {
     res.render('user', data)
   })
 });
-router.post('/register', ensureLoggedIn(),
+router.post('/register',
   function (req, res, next) {
     User.getByUsername(req.body.username)
       .then(function (users) {
@@ -94,36 +76,5 @@ router.post('/register', ensureLoggedIn(),
   function (req, res) {
     req.flash('error', "Couldn't add user.")
     res.redirect('/register')
-  }
-)
-router.post('/order', ensureLoggedIn(),
-  function (req, res, next) {
-    var newOrder = req.body
-    Order.getByOrderNum(newOrder.ordernum)
-      .then(function (orders) {
-        //console.log(orders)
-        //log(orders)
-        if (!_.isEmpty(orders)) {
-          req.flash('error', '该订单号已添加')
-          return res.redirect('/')
-        }
-
-        // req.login() can be used to automatically log the user in after registering
-        console.log(`add new order: user: ${JSON.stringify(req.user)}`)
-        Order.add(newOrder.shopname.trim(), newOrder.keyword.trim(), newOrder.price.trim(), newOrder.ordernum.trim(), req.user.username)
-          .then(function () { return res.redirect('/')} )
-          .catch(function (err) {
-            console.error(err)
-            next()
-          })
-      })
-      .catch(function (err) {
-        console.error(err)
-        next()
-      })
-  },
-  function (req, res) {
-    req.flash('error', "添加新订单失败")
-    res.redirect('/')
   }
 )
---
+++ routes/order.js
@@ -0,0 +1,60 @@
+var router = require('express').Router()
+var Order = require('../models/order')
+var moment = require('moment')
+var _ = require('lodash')
+
+module.exports = router
+
+router.get('/', function (req, res) {
+  console.log('req.query: ', req.query)
+  var isAdmin = req.user.username == 'rosfiled'
+  var data = {
+    error: req.flash('error'),
+    isAdmin,
+    user: req.user,
+    isOrder: true
+  }
+  Order.getByUsername(req.user.username, isAdmin).then((orders) => {
+    var ordersDate = orders.map(order => {
+      return moment(order.created_at).format('YYYY')
+    })
+    console.log(`order dates: ${JSON.stringify(ordersDate)}`)
+    data.orders = orders.map(order => {
+      order.created_at = moment(order.created_at).format('YYYY-MM-D HH:mm:ss')
+      return order
+    });
+    res.render('order', data)
+  })
+});
+
+router.post('/',
+  function (req, res, next) {
+    var newOrder = req.body
+    Order.getByOrderNum(newOrder.ordernum)
+      .then(function (orders) {
+        //console.log(orders)
+        //log(orders)
+        if (!_.isEmpty(orders)) {
+          req.flash('error', '该订单号已添加')
+          return res.redirect('/')
+        }
+
+        // req.login() can be used to automatically log the user in after registering
+        console.log(`add new order: user: ${JSON.stringify(req.user)}`)
+        Order.add(newOrder.shopname.trim(), newOrder.keyword.trim(), newOrder.price.trim(), newOrder.ordernum.trim(), req.user.username)
+          .then(function () { return res.redirect('/')} )
+          .catch(function (err) {
+            console.error(err)
+            next()
+          })
+      })
+      .catch(function (err) {
+        console.error(err)
+        next()
+      })
+  },
+  function (req, res) {
+    req.flash('error', "添加新订单失败")
+    res.redirect('/')
+  }
+)
