f21f58918e132bdd47b8f7ea882b5ed358af94ef
Guanzhou Zhao <benzhao2866@gmail.com>
2017-12-25 23:30:13 +1300

    show user mgmt only for admin

--- routes/task.js
+++ routes/task.js
@@ -3,5 +3,5 @@ var router = require('express').Router()
 module.exports = router
 
 router.get('/', function (req, res) {
-  res.render('task', {user: req.user, isTask: true})
+  res.render('task', {user: req.user, isAdmin:req.user.role == 100, isTask: true})
 });
